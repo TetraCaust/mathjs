637e4ba2f1568fdb968b503df88033be98e1772d
TetraCaust <148044645+TetraCaust@users.noreply.github.com>
2023-11-26 14:49:34 +0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,55 @@
+let a = 3.6;
+let b = 5.5;
+let c = 7.3;
+
+//math round
+let bulatkan = Math.round(a);
+console.log(bulatkan);
+
+let bulatkan2 = Math.round(b);
+console.log(bulatkan2);
+
+let bulatkan3 = Math.round(c);
+console.log(bulatkan3);
+
+//math floor
+let bawah = Math.floor(a);
+console.log(bawah);
+
+let bawah2 = Math.floor(b);
+console.log(bawah2);
+
+let bawah3 = Math.floor(c);
+console.log(bawah3);
+
+//math ceil
+let atas = Math.ceil(a);
+console.log(atas);
+
+let atas2 = Math.ceil(b);
+console.log(atas2);
+
+let atas3 = Math.ceil(c);
+console.log(atas3);
+
+//math pow
+
+let pangkat = Math.pow(2, 3);
+console.log(pangkat);
+
+let pangkat2 = Math.pow(5, 4);
+console.log(pangkat2);
+
+let pangkat3 = Math.pow(3, 5);
+console.log(pangkat3);
+
+//math sqrt
+
+let akar = Math.sqrt(16);
+console.log(akar);
+
+let akar2 = Math.sqrt(256);
+console.log(akar2);
+
+let akar3 = Math.sqrt(81);
+console.log(akar3);
